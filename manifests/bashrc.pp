895c1d6633fb24505f3ea51513e62e820e00829c
Franz Pammer <franz.pammer@knapp.com>
2013-09-26 18:59:10 +0200

    Wrong type for file ist is source

--- manifests/bashrc.pp
+++ manifests/bashrc.pp
@@ -1,21 +1,12 @@
 class bash::bashrc inherits bash::params {
   
-  # Default for new User
-  file { '/etc/skel/.bashrc': 
+  # Default for new User and root
+  file { [ '/etc/skel/.bashrc', '/root/.bashrc' ]: 
     mode    => '0644',
     owner   => 'root',
     group   => 'root',
-    content => "puppet:///modules/${module_name}/bashrc-${osfamily}.sh",
+    source => "puppet:///modules/${module_name}/bashrc-${osfamily}.sh",
     require => Class["bash::install"],
   }
-  
-  # User root
-  file { '/root/.bashrc': 
-    mode    => 0644,
-    owner   => 'root',
-    group   => 'root',
-    content => "puppet:///modules/${module_name}/bashrc-${osfamily}.sh",
-    require => Class["bash::install"],
-  }
-  
+
 }
